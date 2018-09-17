2ee806303249e69a8265063ccbb2643901969363
孙樵 <sunqiaomeisixiang@163.com>
2018-09-17 20:51:36 +0800

    先筑高台，努力拆矮
     微信小程序实践2，把热爱的游戏带到学习中来或许会迸发出更亮丽的火花，就是做了一个简单的英雄联盟精彩视频集锦，其中包括超神篇和沙雕篇，素材均选网上，比起第一个实践——计算器，无论是页面逻辑还是页面美化肯定都要简单许多，但我觉得代码就是要从最简单的写起，一步步的筑起高台，在一步步地将高台拆矮，哇，我觉得这句话说的非常好哇，垒高以达到不断地探索，拆矮以达到不断地精简代码。就这样。

---
+++ lists1/lists1.js
@@ -0,0 +1,88 @@
+// pages/lists1/lists1.js
+Page({
+  data: {
+  videolist:[
+    {
+      id: 1,
+      image: "../../icon/11.jpg",
+      title:"起小点TOP10 VOL431 复仇之矛武术姿态撕裂敌军",
+      date:"2018-09-04 23:27"
+    },
+     {
+      id: 2,
+      image: "../../icon/22.jpg",
+       title: "起小点TOP10 VOL430 刺客之争 影流之主诸刃灭魂",
+       date: "2018-09-01 19:10"
+    },
+    {
+      id: 3,
+      image: "../../icon/33.jpg",
+      title: "起小点TOP10 VOL429 薇恩终极闪避银弩摄魂",
+      date: "2018-08-29 21:15 "
+    },
+    {
+      id: 4,
+      image: "../../icon/44.jpg",
+      title: "起小点TOP10 VOL428 艾克扭转时空化险为夷",
+      date: "2018-08-25 20:40"
+    }
+    
+  ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
